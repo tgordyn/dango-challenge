fc13bd3fbeccfa7c26ae4b27c69adfcbd3234091
tomas gordyn <tomas.gordyn@gmail.com>
2023-09-23 19:40:46 -0300

    correccion fontSize

--- app/utils/fontSizeOptions.js
+++ app/utils/fontSizeOptions.js
@@ -2,5 +2,4 @@ export const options = [
     { value: "xs", label: "Chico" },
     { value: "sm", label: "Mediano" },
     { value: "base", label: "Grande" },
-    { value: "lg", label: "Muy Grande" },
   ];
